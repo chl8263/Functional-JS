892ecea13c6869ca1841ae327481b3ba2bc7653d
Ewan <chl8263@naver.com>
2019-12-15 23:13:37 -0800

    Modify sample

--- 2_3_LessDatastructure,MoreWork.js
+++ 2_3_LessDatastructure,MoreWork.js
@@ -3,10 +3,13 @@ p1.address = new Address("US");
 p1.bithdayYear = 1900;
 
 const p2 = new Person("Barkley", "Rosser", "222-22-2222");
-p2.address = new Address();
+p2.address = new Address("Greece");
+p2.bithdayYear = 1907;
 
+const p3 = new Person("John", "von Neumann", "333-33-3333");
+p3.address = new Address("Hungary");
+p3.bithdayYear = 1903;
 
-const p3 = new Person("Haskell", "curry", "111-11-1111");
-
-
-const p4 = new Person("Haskell", "curry", "111-11-1111");
+const p4 = new Person("Alonzo", "Church", "444-44-4444");
+p2.address = new Address("US");
+p2.bithdayYear = 1903;
