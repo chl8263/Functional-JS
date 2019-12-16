9a07b17db47bb129a5188aef179738dc5d4994c4
Ewan <chl8263@naver.com>
2019-12-15 22:19:34 -0800

    1-2

---
+++ 1_2_highOderFunction.js
@@ -0,0 +1,18 @@
+// 2-3 closure.
+
+var outerVar = 'Outer';
+
+function makeInner(Params){
+    var innervar = "inner";
+
+    function inner(){
+        console.log(
+            `${outervar}, ${innervar}, ${Params}`
+        );
+    }
+
+    return inner;
+}
+
+var inner = makeInner('Params');
+inner();
