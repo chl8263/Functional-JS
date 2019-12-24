b07a54a9b4bd5a61a361fb6245c19a6f4a17ad2f
Ewan <chl8263@naver.com>
2019-12-23 19:49:47 -0800

    compose

--- js/2_4_ForRecycleCode.js
+++ js/2_4_ForRecycleCode.js
@@ -52,3 +52,17 @@ const checkType = R.curry((typeDef, obj) => {
     }
     return obj;
 });
+
+// const logger = new Log4js.getLogger('student');
+// logger.info('asdasd');
+
+//=================== Compose ======================
+const str = `We can only see a short distance ahead but we can see plenty there that needs to be done`;
+
+const explode = (str) => str.split(/\s+/);
+
+const count = (arr) => arr.length;
+
+const countWords = R.compose(count, explode);
+
+console.log(countWords(str));
